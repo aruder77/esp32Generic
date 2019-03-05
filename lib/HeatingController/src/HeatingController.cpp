16402f72cbc49afc1b77dc0c28d09c29d1e89cdf
Axel Ruder <axel.ruder@gmail.com>
2019-03-05 20:01:14 +0100

    added HeatingController

---
+++ lib/HeatingController/src/HeatingController.cpp
@@ -0,0 +1,19 @@
+#include "HeatingController.h"
+
+HeatingController::HeatingController() {
+    prefs->registerConfigParam("atFuehlerPin", "Außentemperatur-Fühler-Pin", "34", 3, this);
+}
+
+void HeatingController::setup() {
+    atFuehlerPin = prefs->getInt("atFuehlerPin");
+
+}
+
+void HeatingController::configUpdate(const char *id, const char *value) {
+
+}
+
+void HeatingController::everySecond() {
+    uint16_t adc = analogRead(atFuehlerPin);
+    Log.notice("ADC value: %d\n", adc);
+}
---
+++ lib/HeatingController/src/HeatingController.h
@@ -0,0 +1,23 @@
+#ifndef HEATING_CONTROLLER_H_
+#define HEATING_CONTROLLER_H_
+
+#include <Module.h>
+#include <Prefs.h>
+#include <ArduinoLog.h>
+
+class HeatingController : public Module, public PrefsClient {
+
+    public:
+        HeatingController();
+
+        void setup();
+        void everySecond();
+
+        void configUpdate(const char *id, const char *value);
+
+    private:
+        Prefs *prefs = Prefs::getInstance();
+        int atFuehlerPin = 34;
+}
+
+#endif
